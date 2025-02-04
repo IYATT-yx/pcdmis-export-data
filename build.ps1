f3c53e51015f52d2dd18b27b29c57ee6dc7794b3
IYATT-yx <iyatt@iyatt.com>
2025-02-04 22:39:57 +0800

    根据功能变更，对构建脚本调整适配

--- build.ps1
+++ build.ps1
@@ -2,4 +2,8 @@ python -m venv venv
 .\venv\Scripts\Activate.ps1
 pip install -r requirements.txt
 pip install pyinstaller
-pyinstaller -F -i .\icon.ico --add-data '.\icon.ico;.' pcdmis-export-data.py
+
+python .\savebuildtime.py
+pyinstaller -F -i .\icon.ico --add-data '.\icon.ico;.' --add-data 'buildTime.txt;.' `
+--hidden-import=pcdlrnconst.pcdlrnconst2019R2 --hidden-import=pcdlrnconst.pcdlrnconst20232 `
+pcdmis-export-data.py
