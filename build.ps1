fe94f85dae1c6a2bd5ac1f419fdb7eab350ce8e3
IYATT-yx <iyatt@iyatt.com>
2025-02-06 22:23:36 +0800

    打包工具由 pyinstaller 改为 nuitka

--- build.ps1
+++ build.ps1
@@ -1,9 +1,19 @@
+$startTime = Get-Date
+
 python -m venv venv
 .\venv\Scripts\Activate.ps1
 pip install -r requirements.txt
-pip install pyinstaller
+pip install nuitka==2.6.4
 
 python .\savebuildtime.py
-pyinstaller -F -i .\icon.ico --add-data '.\icon.ico;.' --add-data 'buildTime.txt;.' `
---hidden-import=pcdlrnconst.pcdlrnconst2019R2 --hidden-import=pcdlrnconst.pcdlrnconst20232 `
-pcdmis-export-data.py
+
+nuitka --standalone --onefile `
+--include-module=pcdlrnconst.pcdlrnconst20232 --include-module=pcdlrnconst.pcdlrnconst2019R2 `
+--enable-plugin=tk-inter --windows-icon-from-ico=./icon.ico --include-data-file=./icon.ico=.\ `
+--include-data-files=.\icon.ico=.\ `
+--output-dir=dist --output-filename=pcdmis-export-data_win_amd64 `
+.\pcdmis-export-data.py
+
+$endTime = Get-Date
+$elapsedTime = New-TimeSpan -Start $startTime -End $endTime
+Write-Output "程序构建用时：$($elapsedTime.TotalSeconds) 秒"
