a640249134457f91212784d7f894246651c51ec9
IYATT-yx <iyatt@iyatt.com>
2026-09-06 01:00:32 +0800

    升级 nuitka 版本

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ python -m venv venv
 .\venv\Scripts\Activate.ps1
 python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
-pip install nuitka==4.1.2
+pip install nuitka==4.2
 
 python .\savebuildtime.py
 
@@ -16,7 +16,7 @@ nuitka --standalone `
 --enable-plugin=tk-inter `
 --windows-company-name="IYATT-yx" `
 --windows-product-name="PC-DMIS 数据导出工具" `
---windows-file-description="PC-DMIS 测量数据自动化导出程序" `
+--windows-file-description="PC-DMIS 数据导出工具" `
 --windows-product-version="1.0.0.0" `
 --windows-file-version="1.0.0.0" `
 --copyright="Copyright (C) 2026 IYATT-yx. All Rights Reserved." `
