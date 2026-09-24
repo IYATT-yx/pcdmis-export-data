fd2de8b1d6e31230174ec756c522a957fae84bdf
IYATT-yx <iyatt@iyatt.com>
2026-09-24 21:47:00 +0800

    构建时间记录方法修改

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 pip install nuitka==4.2
 
-python .\savebuildtime.py
+Set-Content -Path ".\buildtime.py" -Value "buildTime = '$(Get-Date -Format 'yyyyMMdd_HHmmss')'" -Encoding UTF8
 
 nuitka --standalone `
 --windows-uac-admin `
